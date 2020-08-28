b4d3fcb50dc9b963960403c591beebe6a18e4df7
yclimb <panyi@iyuedian.com>
2020-08-28 18:12:37 +0800

    求数组的中间节点

---
+++ src/main/java/com/yclimb/array/MiddleNode.java
@@ -0,0 +1,17 @@
+package com.yclimb.array;
+
+/**
+ * 求数组的中间节点
+ *
+ * @author yclimb
+ * @date 2020/8/28
+ */
+public class MiddleNode {
+    public static void main(String[] args) {
+
+        String[] strs = new String[]{"1","2","3","4"};
+        int l = strs.length / 2;
+        System.out.println(strs[l]);
+
+    }
+}
