80f96a196b7b763971090f4b29036bb8e7c53934
yclimb <panyi@iyuedian.com>
2020-09-01 16:36:02 +0800

    递归

---
+++ recursion/Recursion.java
@@ -0,0 +1,67 @@
+package recursion;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 递归
+ *
+ * @author yclimb
+ * @date 2020/8/27
+ */
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println(f(5));
+        System.out.println(f2(5));
+        System.out.println(f3(5));
+    }
+
+    /**
+     * 假如你站在 n 个台阶上，从这里出发去到第 1 个台阶，一次只能跨 1 个台阶，请问最后跨了多少步？
+     * @param n n
+     * @return int
+     */
+    public static int f(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return f(n - 1) + 1;
+    }
+
+    /**
+     * 假如这里有 n 个台阶，每次你可以跨 1 个台阶或者 2 个台阶，请问走这 n 个台阶有多少种走法？
+     * @param n n
+     * @return int
+     */
+    public static int f2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return f2(n-1) + f2(n-2);
+    }
+
+    static Map<Integer, Integer> hasSolvedList = new HashMap<>();
+    /**
+     * f2 避免重复计算的递归
+     * @param n n
+     * @return int
+     */
+    public static int f3(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        // hasSolvedList可以理解成一个Map，key是n，value是f(n)
+        if (hasSolvedList.containsKey(n)) {
+            return hasSolvedList.get(n);
+        }
+        int ret = f3(n - 1) + f3(n - 2);
+        hasSolvedList.put(n, ret);
+        return ret;
+    }
+}
+
