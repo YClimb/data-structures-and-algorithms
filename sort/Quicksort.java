05333a490512b72f6cf5999316dd8e43ad20e64c
yclimb <panyi@iyuedian.com>
2020-09-04 16:55:06 +0800

    快速排序

--- sort/Quicksort.java
+++ sort/Quicksort.java
@@ -13,6 +13,10 @@ public class Quicksort {
          * https://github.com/wangzheng0822/algo/blob/master/java/12_sorts/QuickSort.java
          * 快速排序算法（Quicksort），我们习惯性把它简称为“快排”。快排利用的也是分治思想。
          * 快排的思想是这样的：如果要排序数组中下标从 p 到 r 之间的一组数据，我们选择 p 到 r 之间的任意一个数据作为 pivot（分区点）。
+         * 我们遍历 p 到 r 之间的数据，将小于 pivot 的放到左边，将大于 pivot 的放到右边，将 pivot 放到中间。
+         * 经过这一步骤之后，数组 p 到 r 之间的数据就被分成了三个部分，
+         * 前面 p 到 q-1 之间都是小于 pivot 的，中间是 pivot，后面的 q+1 到 r 之间是大于 pivot 的。
+         * 根据分治、递归的处理思想，我们可以用递归排序下标从 p 到 q-1 之间的数据和下标从 q+1 到 r 之间的数据，直到区间缩小为 1，就说明所有的数据都有序了。
          */
     }
 
