41528f3fa104914cde2682862e0b9f2ff0842369
yclimb <panyi@iyuedian.com>
2020-09-04 15:45:02 +0800

    归并排序

---
+++ sort/MergeSort.java
@@ -0,0 +1,18 @@
+package sort;
+
+/**
+ * 归并排序
+ *
+ * @author yclimb
+ * @date 2020/9/4
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        /**
+         * https://time.geekbang.org/column/article/41913
+         * https://github.com/wangzheng0822/algo/blob/master/java/12_sorts/MergeSort.java
+         * 归并排序的核心思想。
+         * 如果要排序一个数组，我们先把数组从中间分成前后两部分，然后对前后两部分分别排序，再将排好序的两部分合并在一起，这样整个数组就都有序了。
+         */
+    }
+}
