39fb9ddaf165cf8b9479be8a45bd4d75fca23fd5
yclimb <panyi@iyuedian.com>
2020-09-04 16:39:49 +0800

    线性排序

---
+++ sort/LinearSort.java
@@ -0,0 +1,19 @@
+package sort;
+
+/**
+ * 线性排序
+ *
+ * @author yclimb
+ * @date 2020/9/4
+ */
+public class LinearSort {
+    public static void main(String[] args) {
+        /**
+         * https://time.geekbang.org/column/article/42038
+         * https://github.com/wangzheng0822/algo/tree/master/java/13_sorts
+         * 三种时间复杂度是 O(n) 的排序算法：桶排序[BucketSort]、计数排序[CountingSort]、基数排序[RadixSort]。
+         * 因为这些排序算法的时间复杂度是线性的，所以我们把这类排序算法叫作线性排序（Linear sort）。
+         * 之所以能做到线性的时间复杂度，主要原因是，这三个算法是非基于比较的排序算法，都不涉及元素之间的比较操作。
+         */
+    }
+}
