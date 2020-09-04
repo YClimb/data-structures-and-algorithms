8903793c0b985c32e4375c2a82b7dae6d29cf00c
yclimb <panyi@iyuedian.com>
2020-09-04 11:19:19 +0800

    插入排序

--- sort/InsertionSort.java
+++ sort/InsertionSort.java
@@ -8,7 +8,14 @@ package sort;
  */
 public class InsertionSort {
     public static void main(String[] args) {
-
+        /*
+         * https://time.geekbang.org/column/article/41802
+         * 那插入排序具体是如何借助上面的思想来实现排序的呢？
+         * 首先，我们将数组中的数据分为两个区间，已排序区间和未排序区间。
+         * 初始已排序区间只有一个元素，就是数组的第一个元素。
+         * 插入算法的核心思想是取未排序区间中的元素，在已排序区间中找到合适的插入位置将其插入，并保证已排序区间数据一直有序。
+         * 重复这个过程，直到未排序区间中元素为空，算法结束。
+         */
     }
 
     /**
