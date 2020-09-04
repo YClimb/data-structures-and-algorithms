25ff5101691c751fb886455c9fa2583ac018627f
yclimb <panyi@iyuedian.com>
2020-09-04 11:20:15 +0800

    冒泡排序

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -8,7 +8,13 @@ package sort;
  */
 public class BubbleSort {
     public static void main(String[] args) {
-
+        /*
+         * https://time.geekbang.org/column/article/41802
+         * 冒泡排序只会操作相邻的两个数据。
+         * 每次冒泡操作都会对相邻的两个元素进行比较，看是否满足大小关系要求。
+         * 如果不满足就让它俩互换。
+         * 一次冒泡会让至少一个元素移动到它应该在的位置，重复 n 次，就完成了 n 个数据的排序工作。
+         */
     }
 
     /**
