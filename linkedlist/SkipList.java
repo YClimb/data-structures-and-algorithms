c05b2fd0e2f68e1cadcffbf6ae925d4c8bc5e804
yclimb <panyi@iyuedian.com>
2020-09-07 17:22:10 +0800

    跳表

---
+++ linkedlist/SkipList.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+/**
+ * 跳表
+ *
+ * @author yclimb
+ * @date 2020/9/7
+ */
+public class SkipList {
+    public static void main(String[] args) {
+        /**
+         * https://time.geekbang.org/column/article/42896
+         * https://github.com/wangzheng0822/algo/tree/master/java/17_skiplist
+         * 跳表：链表上加多级索引的结构，就是跳表。
+         * 跳表使用空间换时间的设计思路，通过构建多级索引来提高查询的效率，实现了基于链表的“二分查找”。
+         * 跳表是一种动态数据结构，支持快速地插入、删除、查找操作，时间复杂度都是 O(logn)。
+         *
+         */
+    }
+}
