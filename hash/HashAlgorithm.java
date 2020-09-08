7e00499f3bb414e3f8552304a766a44ce02f40d2
yclimb <panyi@iyuedian.com>
2020-09-08 17:22:19 +0800

    Hash算法

---
+++ hash/HashAlgorithm.java
@@ -0,0 +1,36 @@
+package hash;
+
+/**
+ * Hash算法
+ *
+ * @author yclimb
+ * @date 2020/9/8
+ */
+public class HashAlgorithm {
+    public static void main(String[] args) {
+        /**
+         * 什么是哈希算法？
+         * 将任意长度的二进制值串映射为固定长度的二进制值串，这个映射的规则就是哈希算法，而通过原始数据映射之后得到的二进制值串就是哈希值。
+         *
+         * 哈希算法的七个应用，它们分别是：安全加密、数据校验、唯一标识、散列函数、负载均衡、数据分片、分布式存储。
+         * https://time.geekbang.org/column/article/65312
+         *
+         * 安全加密：
+         * 最常用于加密的哈希算法是 MD5（MD5 Message-Digest Algorithm，MD5 消息摘要算法）和 SHA（Secure Hash Algorithm，安全散列算法）。除了这两个之外，当然还有很多其他加密算法，比如 DES（Data Encryption Standard，数据加密标准）、AES（Advanced Encryption Standard，高级加密标准）。
+         *
+         * 数据校验：
+         * 唯一标识：
+         * 散列函数：
+         * https://time.geekbang.org/column/article/65312
+         *
+         * 负载均衡：
+         * 数据分片：
+         * 分布式存储：
+         * https://time.geekbang.org/column/article/67388
+         *
+         * 题外话：一致性hash算法，使用hash环来解决节点新增删除带来的hash雪崩影响
+         * www.zsythink.net/archives/1182
+         *
+         */
+    }
+}
