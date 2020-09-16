ce36136c9f26a326ac3bdf613d874636c8b999a8
yclimb <panyi@iyuedian.com>
2020-09-16 13:52:03 +0800

    图

--- graph/Graph.java
+++ graph/Graph.java
@@ -37,4 +37,23 @@ public class Graph {
          *
          */
     }
+
+
+    /*public class Graph { // 无向图
+        private int v; // 顶点的个数
+        private LinkedList<Integer> adj[]; // 邻接表
+
+        public Graph(int v) {
+            this.v = v;
+            adj = new LinkedList[v];
+            for (int i=0; i<v; ++i) {
+                adj[i] = new LinkedList<>();
+            }
+        }
+
+        public void addEdge(int s, int t) { // 无向图一条边存两次
+            adj[s].add(t);
+            adj[t].add(s);
+        }
+    }*/
 }
