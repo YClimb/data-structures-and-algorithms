fae2b5417e1eddf500efa1c6ac116474b3251afd
yclimb <yclimb@qq.com>
2020-09-17 12:30:07 +0800

    test commit

--- demo/Demo.java
+++ demo/Demo.java
@@ -8,6 +8,6 @@ package demo;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("22222");
+        System.out.println("test github commit.");
     }
 }
