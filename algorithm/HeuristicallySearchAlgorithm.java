ccd9d0e3f38d70e96b5c53fccabcebad3a0c73e4
yclimb <yclimb@qq.com>
2020-09-24 15:24:59 +0800

    启发式搜索算法（Heuristically Search Algorithm）

---
+++ algorithm/HeuristicallySearchAlgorithm.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+/**
+ * 启发式搜索算法（Heuristically Search Algorithm）
+ *
+ * @author yclimb
+ * @date 2020/9/24
+ */
+public class HeuristicallySearchAlgorithm {
+    public static void main(String[] args) {
+        /**
+         * https://time.geekbang.org/column/article/78175
+         * 启发式搜索算法（Heuristically Search Algorithm）
+         *
+         * A* 算法属于一种启发式搜索算法（Heuristically Search Algorithm）。
+         * 实际上，启发式搜索算法并不仅仅只有 A* 算法，还有很多其他算法，比如 IDA* 算法、蚁群算法、遗传算法、模拟退火算法等。
+         *
+         * 启发式搜索算法利用估价函数，避免“跑偏”，贪心地朝着最有可能到达终点的方向前进。
+         * 这种算法找出的路线，并不是最短路线。但是，实际的软件开发中的路线规划问题，我们往往并不需要非得找最短路线。
+         * 所以，鉴于启发式搜索算法能很好地平衡路线质量和执行效率，它在实际的软件开发中的应用更加广泛。
+         *
+         */
+    }
+}
